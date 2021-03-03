750b67344e7f06ae213b251cd496e9e433aada9e
ivona99 <57632353+ivona99@users.noreply.github.com>
2021-03-03 22:14:04 +0100

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,5 +1,10 @@
 <?php  
-
+$errors = array();
+$c_name = "";
+$c_email    = "";
 $con= mysqli_connect("localhost", "root", "", "ecom_store");
+if(!$con){
+    die('Conection failed : ' .mysqli_connect_error());
+}
 
 ?>
